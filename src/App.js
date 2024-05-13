62eeb23c936560ac43ed1cdb42d6a76dad39bb3f
Arumugaperumal N <arumugaperumal0007@gmail.com>
2024-05-13 13:49:49 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./Home";
+import Food from "./Food";
+const App = () => {
+  return (
+    <div className="App">
+      <div>
+      <Router>
+      <Routes>
+      <Route path="/food" element={<Food />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/Food-recipe-finder" element={<Home />} />
+      </Routes>
+        </Router>
+     
+      </div>
+     
+    </div>
+  );
+}
+
+export default App;
